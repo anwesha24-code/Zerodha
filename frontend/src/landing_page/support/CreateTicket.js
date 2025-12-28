6278b4f112de00a3280d213d711a692a431ef70c
Anwesha Pal <anweshapal2006@gmail.com>
2025-12-28 11:58:16 +0530

    support page made

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,8 +1,82 @@
 import React from 'react';
 function CreateTicket() {
-    return ( 
-        <h1>CreateTicket</h1>
-     );
+    return (
+        <div className="container">
+            <div className='row p-5 mt-5 mb-5'>
+                <h1 className='fs-2'>To create a ticket, select a relevant category and describe your issue in detail.</h1>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Openings</h4>
+
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Online Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Offline Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Company, Partnership Firm Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>OpeningsNRI Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Charges at Zerodha</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Getting Started</a><br />
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Openings</h4>
+
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Online Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Offline Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Company, Partnership Firm Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>OpeningsNRI Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Charges at Zerodha</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Getting Started</a><br />
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Openings</h4>
+
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Online Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Offline Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Company, Partnership Firm Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>OpeningsNRI Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Charges at Zerodha</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Getting Started</a><br />
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Openings</h4>
+
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Online Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Offline Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Company, Partnership Firm Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>OpeningsNRI Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Charges at Zerodha</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Getting Started</a><br />
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Openings</h4>
+
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Online Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Offline Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Company, Partnership Firm Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>OpeningsNRI Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Charges at Zerodha</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Getting Started</a><br />
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Openings</h4>
+
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Online Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Offline Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Company, Partnership Firm Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>OpeningsNRI Account Openings</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Charges at Zerodha</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                    <a href="" style={{ textDecoration: 'none', lineWeight: '2.5' }}>Getting Started</a><br />
+                </div>
+
+
+            </div>
+
+
+        </div>
+    );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,8 +1,32 @@
 import React from 'react';
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        // container-fluid stretches the container end-to-end
+        <div className="container-fluid" id="supportHero">
+            
+            <div className='p-5' id="supportWrapper">
+                <h4 className='fs-2'>Support Portal</h4>
+                <a href="" className='fs-5'>Track Ticket</a>
+            </div>
+            <div className='row p-5 m-5'>
+                <div className='col-6 p-5'>
+                    <h1 className='fs-3 pb-3'>Search for an answer or above help topics<br/>to create a ticket</h1>
+                    <input placeholder='Eg. how do I activate F&O'/><br/>
+                    <a href=''style={{color:'white'}}>Track account Openings</a><br/>
+                    <a href='' style={{color:'white'}}>Track segment activation</a><br/>
+                    <a href='' style={{color:'white'}}>Intraday margins</a><br/>
+                    <a href='' style={{color:'white'}}>Kite user manual</a><br/>
+                </div>
+                <div className='col-6 p-5 ml-5'>
+                    <h1 className='fs-3'> Featured</h1>
+                    <ol>
+                        <li><a href='' style={{color:'white'}}>Current Takeovers and Delisting - January 2024</a></li>
+                        <li><a href='' style={{color:'white'}}>Latest Intraday leverages - MIS & CO</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
