77b0115b72e80d7b4d0ecf6e389824412eb9b3e2
Anwesha Pal <anweshapal2006@gmail.com>
2025-12-30 12:42:00 +0530

    comments added

--- backend/src/components/WatchList.js
+++ backend/src/components/WatchList.js
@@ -29,21 +29,31 @@ const WatchList = () => {
 };
 export default WatchList;
 
-//we can make the watchlist item outside in another file also but since this is very tightly coupoled and has no use outside of this file we write 
+//we can make the watchlist item outside in another file also but since this is very tightly
+// coupled and has no use outside of this file we write 
 //watchlist item component here itself
 
+//this is the part that will always be visible
+
 const WatchListItem = ({ stock }) => {
-  const [showWatchlistActions, setShowWatchlistActions] = useState(false);//to keep track of hover so that buy sell button appears
-  const handleMouseEnter = (e) => {
+  //to keep track of hover so that buy sell button appears
+  const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+
+  const handleMouseEnter = (e) => {//when hover
     setShowWatchlistActions(true);
   }
-  const handleMouseExit = (e) => {
+  const handleMouseExit = (e) => {//when hover out
     setShowWatchlistActions(false);
   }
   return (
+    //when mouse enters we call handleMouseEnter so that showWatchlistActions becomes true and buy sell buttons appear
+    //when mouse exits we call handleMouseExit so that showWatchlistActions becomes false and buy sell buttons disappear
+
     <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
       <div className="item">
+        {/*if down then the stock name will be red else green className down will make it red and up will make it green*/}
         <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+
         <div className="itemInfo">
           <span className="percent">{stock.percent}</span>
           {stock.isDown ? (
@@ -53,14 +63,19 @@ const WatchListItem = ({ stock }) => {
           )}
           <span className="price">{stock.price}</span>
         </div>
+
       </div>
+
+      {/*will be visible on top of li content when hovered*/}
       {showWatchlistActions && <WatchlistActions uid={stock.name} />}
     </li>
   );
 };
 
 //visible only on hover
-const WatchlistActions = ({uid}) => {//stock name is used as uid {} is destructure
+
+const WatchlistActions = ({uid}) => {//stock name is used as uid, {} is for destructuring
+  //for each stock we have buy sell analytics more buttons
   return (
     <span className="actions">
       <span>
